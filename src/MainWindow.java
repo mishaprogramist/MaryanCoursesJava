adf2088e0fce873b6668faae865e07bc59dfa806
mishaprogramist <com2002@list.ru>
2015-09-30 23:04:43 +0300

    "Hello World" перенесен в отдельный класс

---
+++ src/MainWindow.java
@@ -0,0 +1,16 @@
+/**
+ * Created by m.karma on 30.09.2015.
+ */
+public class MainWindow {
+    public MainWindow(){
+
+    }
+
+    public void show(){
+        String str = "Hello World";
+
+        System.out.println(str);
+    }
+
+
+}
--- src/MaryanCoursesJava.java
+++ src/MaryanCoursesJava.java
@@ -2,10 +2,10 @@
  * Created by m.karma on 30.09.2015.
  */
 public class MaryanCoursesJava {
+
     public static void main(String [] args) {
 
-        String str = "Hello World";
-
-        System.out.println(str);
+        MainWindow mw = new MainWindow();
+        mw.show();
     }
 }
